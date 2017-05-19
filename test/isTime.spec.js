9c41a010c9e8eb6ca4b5cca5e5c558ad9dfc5839
Perry Mitchell <perry@perrymitchell.net>
2017-05-19 20:15:39 +0300

    Detect some invalid times

--- test/isTime.spec.js
+++ test/isTime.spec.js
@@ -6,15 +6,19 @@ const { valid, invalid } = require("./times.json");
 describe("isTime", function() {
 
     describe("when checking valid times", function() {
-
         valid.forEach(function(time) {
-
             it(`recognises '${time}' as valid`, function() {
                 expect(isTime(time)).to.be.true;
             });
-
         });
+    });
 
+    describe("when checking invalid times", function() {
+        invalid.forEach(function(time) {
+            it(`recognises '${time}' as invalid`, function() {
+                expect(isTime(time)).to.be.false;
+            });
+        });
     });
 
 });
